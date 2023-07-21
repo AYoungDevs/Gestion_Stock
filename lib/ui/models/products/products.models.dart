953d7ce6478cfba983bc3870b53877304eedf4cd
Michel.Ahoba <michel.ahoba@intuitcorporte.com>
2023-07-21 21:48:35 +0000

    Dashboard en cours

---
+++ lib/ui/models/products/products.models.dart
@@ -0,0 +1,103 @@
+// ignore_for_file: non_constant_identifier_names, unused_import
+
+import 'package:flutter/material.dart';
+
+class Products {
+  String image, title, price;
+
+  Products({
+    required this.image,
+    required this.title,
+    required this.price,
+  });
+
+  // static List<Products> items() {
+  //   return List.generate(
+  //     20,
+  //     (index) => Products(
+  //       title: "COCA-COLA",
+  //       image: "assets/images/coca_cola_33cl.jpg",
+  //       price: "1000FCFA",
+  //     //     ),
+  //     growable: true,
+  //   );
+  // }
+}
+
+List<Products> products = [
+  Products(
+    title: "COCA-COLA",
+    image: "assets/images/coca_cola_33cl.jpg",
+    price: "1000FCFA",
+  ),
+  Products(
+    title: "COCA-COLA",
+    image: "assets/images/coca_cola_33cl.jpg",
+    price: "1000FCFA",
+  ),
+  Products(
+    title: "COCA-COLA",
+    image: "assets/images/coca_cola_33cl.jpg",
+    price: "1000FCFA",
+  ),
+  Products(
+    title: "COCA-COLA",
+    image: "assets/images/coca_cola_33cl.jpg",
+    price: "1000FCFA",
+  ),
+  Products(
+    title: "COCA-COLA",
+    image: "assets/images/coca_cola_33cl.jpg",
+    price: "1000FCFA",
+  ),
+  Products(
+    title: "COCA-COLA",
+    image: "assets/images/coca_cola_33cl.jpg",
+    price: "1000FCFA",
+  ),
+  Products(
+    title: "COCA-COLA",
+    image: "assets/images/coca_cola_33cl.jpg",
+    price: "1000FCFA",
+  ),
+  Products(
+    title: "COCA-COLA",
+    image: "assets/images/coca_cola_33cl.jpg",
+    price: "1000FCFA",
+  ),
+  Products(
+    title: "COCA-COLA",
+    image: "assets/images/coca_cola_33cl.jpg",
+    price: "1000FCFA",
+  ),
+  Products(
+    title: "COCA-COLA",
+    image: "assets/images/coca_cola_33cl.jpg",
+    price: "1000FCFA",
+  ),
+  Products(
+    title: "COCA-COLA",
+    image: "assets/images/coca_cola_33cl.jpg",
+    price: "1000FCFA",
+  ),
+  Products(
+    title: "COCA-COLA",
+    image: "assets/images/coca_cola_33cl.jpg",
+    price: "1000FCFA",
+  ),
+  Products(
+    title: "COCA-COLA",
+    image: "assets/images/coca_cola_33cl.jpg",
+    price: "1000FCFA",
+  ),
+  Products(
+    title: "COCA-COLA",
+    image: "assets/images/coca_cola_33cl.jpg",
+    price: "1000FCFA",
+  ),
+  Products(
+    title: "COCA-COLA",
+    image: "assets/images/coca_cola_33cl.jpg",
+    price: "1000FCFA",
+  ),
+];
