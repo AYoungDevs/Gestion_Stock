89f1d5742d7f0cf311d2b8dd044ce26b362abf0b
Croco <lacosteouattara1@gmail.com>
2023-08-06 02:48:25 +0000

    modif profil1

--- lib/ui/screens/profil/profil.dart
+++ lib/ui/screens/profil/profil.dart
@@ -20,6 +20,13 @@ class _ProfilScreenState extends State<ProfilScreen> {
 
     // Récupérer la hauteur de l'écran
     double screenHeight = screenSize.height;
+
+    // int _selectedValue = 1;
+
+    // List<Map<String, dynamic>> _radioValues = [
+    //   {"label": "Homme", "value": 1},
+    //   {"label": "Femme", "value": 2},
+    // ];
     return Scaffold(
       body: SafeArea(
         child: Row(
@@ -33,11 +40,11 @@ class _ProfilScreenState extends State<ProfilScreen> {
                 child: Row(
                   children: [
                     Padding(
-                      padding:
-                          EdgeInsets.only(left: 20, right: 45, bottom: 150),
+                      padding: EdgeInsets.only(
+                          left: 20, right: 25, bottom: 150, top: 24),
                       child: Container(
-                          width: screenWidth * 0.2,
-                          height: screenHeight * 0.5,
+                          width: screenWidth / 4,
+                          height: screenHeight * 0.7,
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(50),
                             color: profilColor1,
@@ -58,17 +65,115 @@ class _ProfilScreenState extends State<ProfilScreen> {
                               Text(
                                 "Michel Ahoba",
                                 style: TextStyle(
-                                    color: Colors.black, fontSize: 20),
+                                    color: Colors.black, fontSize: 32),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 10),
+                                child: InkWell(
+                                  onTap: () => {},
+                                  child: Text(
+                                    "Informations personnelles",
+                                    style: TextStyle(
+                                        color: primaryColor,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 10),
+                                child: InkWell(
+                                  onTap: () => {},
+                                  child: Text(
+                                    "Demande de modification",
+                                    style: TextStyle(
+                                      color: primaryColor,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 10),
+                                child: InkWell(
+                                  onTap: () => {},
+                                  child: Text(
+                                    "Mot de passe oublié",
+                                    style: TextStyle(
+                                      color: primaryColor,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ),
                               ),
                             ],
                           )),
                     ),
                     Padding(
-                      padding: EdgeInsets.only(right: 0),
+                      padding: EdgeInsets.only(top: 38, bottom: 24),
                       child: Container(
-                        width: 750,
-                        height: 650,
+                        width: screenWidth * 0.58,
+                        height: screenHeight * 1,
                         color: profilColor12,
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.only(top: 29, left: 48),
+                                  child: Row(
+                                    children: [
+                                      Text(
+                                        "Homme",
+                                        style: TextStyle(
+                                            color: Colors.black, fontSize: 22),
+                                      ),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      // Row(
+                                      //   children: _radioValues.map((option) {
+                                      //     return RadioListTile(
+                                      //       title: Text(option["label"]),
+                                      //       value: option["value"],
+                                      //       groupValue: _selectedValue,
+                                      //       onChanged: (value) {
+                                      //         setState(() {
+                                      //           _selectedValue = value;
+                                      //         });
+                                      //       },
+                                      //     );
+                                      //   }).toList(),
+                                      // ),
+                                      Radio(
+                                        value: Radio,
+                                        groupValue: ActionListener,
+                                        onChanged: (value) => '1',
+                                      ),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Text(
+                                        "Femme",
+                                        style: TextStyle(
+                                            color: Colors.black, fontSize: 22),
+                                      ),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Radio(
+                                        value: Radio,
+                                        groupValue: ActionListener,
+                                        onChanged: (value) => '1',
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
