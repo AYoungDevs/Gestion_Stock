ce442a0efd7fa122a3ab399bc99820fb28027f0e
ydev19 <yannsebeo005@gmail.com>
2023-08-18 14:57:19 +0000

    interface products en cours

--- lib/ui/screens/products/addproducts.dart
+++ lib/ui/screens/products/addproducts.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../styles/colors.style.dart';
+
 class AddProducts extends StatefulWidget {
   @override
   State<AddProducts> createState() {
@@ -12,37 +14,204 @@ class _AddProductsState extends State<AddProducts> {
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     return AlertDialog(
+      backgroundColor: pageBackgroundColor,
       content: Column(
         children: [
           Container(
             width: screenSize.width / 2,
             decoration: BoxDecoration(
-                color: Colors.white,
+                color: pageBackgroundColor,
                 borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(4), topRight: Radius.circular(4))),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Text('Nouveau Produit',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
                 Spacer(),
                 InkWell(
                     onTap: () {
                       Navigator.of(context).pop(context);
                     },
                     child: Container(
-                        decoration: BoxDecoration(
+                        decoration: const BoxDecoration(
                             shape: BoxShape.circle, color: Colors.red),
                         child: Icon(
                           Icons.close,
                           color: Colors.white,
                           size: 25,
-                        )))
+                        ))),
               ],
             ),
+          ),
+          SizedBox(height: 40),
+          Form(
+            child: Row(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      width: screenSize.width / 5,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: "Nom produit",
+                          labelStyle: TextStyle(color: primaryColor),
+                          filled: true,
+                          fillColor: secondaryColor,
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: primaryColor),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: secondaryColor),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Container(
+                  // DropdownSearch<String>(
+                  // items: const ["Alto", "Basse", "Soprano", 'Tenor'],
+                  // dropdownDecoratorProps: const DropDownDecoratorProps(
+                  // dropdownSearchDecoration: InputDecoration(
+                  // filled: true,
+                  // fillColor: Color.fromARGB(255, 255, 255, 255),
+                  // border: OutlineInputBorder(
+                  // borderSide: BorderSide(color: Colors.black)),
+                  // labelText: "choisir ta voix",
+                  // ),
+                  // ),
+                  // onChanged: print,
+                  // selectedItem: "",
+                  // ),
+                  width: screenSize.width / 5,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: "Selectionner la categorie",
+                      labelStyle: TextStyle(color: primaryColor),
+                      filled: true,
+                      fillColor: secondaryColor,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: primaryColor),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: secondaryColor),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 60),
+          Row(
+            children: [
+              Container(
+                width: screenSize.width / 5,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "Prix d'achat",
+                    labelStyle: TextStyle(color: primaryColor),
+                    filled: true,
+                    fillColor: secondaryColor,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: primaryColor),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: secondaryColor),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              Spacer(),
+              Container(
+                width: screenSize.width / 5,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "Prix de revente",
+                    labelStyle: TextStyle(color: primaryColor),
+                    filled: true,
+                    fillColor: secondaryColor,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: primaryColor),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: secondaryColor),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 60),
+          TextField(
+            decoration: InputDecoration(
+              labelText: "Quantité",
+              labelStyle: TextStyle(color: primaryColor),
+              filled: true,
+              fillColor: secondaryColor,
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: primaryColor),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: secondaryColor),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text("Image"),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              InkWell(
+                onTap: () {
+                  print('photo');
+                  //_selectedfilesImage()
+                },
+                child: const Icon(
+                  Icons.photo,
+                  size: 80,
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 60),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("Enregister"),
           ),
         ],
       ),
     );
   }
+
+  //_selectedfilesImage() async {
+  // result = await FilePicker.platform.pickFiles();
+  //if (result != null) {
+  //file = File(result?.files.single.path as String);
+  //setState(() {
+  //image.text = file!.path;
+  //});
+  // } else {}
+  // }
 }
--- lib/ui/widget/acceuil/acceuil.products.dart
+++ lib/ui/widget/acceuil/acceuil.products.dart
@@ -20,86 +20,165 @@ class AcceuilScreenProductsState extends State<AcceuilScreenProducts> {
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
+    //List<InfoProducts> infoproducts = [];
+
+    // void iniState() {
+    // infoproducts = InfoProducts.getInfoProducts();
+    //super.initState();
+    //}
+
+    List<DataCell> Contenu = ([
+      DataCell(Text("1")),
+      DataCell(Container(
+          child: Image.asset('assets/images/coca_cola_33cl.jpg',
+              height: 40, width: 80))),
+      DataCell(Text('Sucrerie')),
+      DataCell(Text('50')),
+      DataCell(Text('13/05/2023')),
+      DataCell(Row(
+        children: [
+          InkWell(
+              onTap: () {
+                print('supprimé');
+              },
+              child: Icon(
+                Icons.delete_sharp,
+                color: Colors.red,
+              )),
+          SizedBox(width: 25),
+          InkWell(
+              onTap: () {
+                print("editer");
+              },
+              child: Icon(
+                Icons.edit,
+                color: facebookColor,
+              ))
+        ],
+      )),
+    ]);
+
+    List<DataColumn> EnteteTableau = ([
+      DataColumn(label: Text('#ID')),
+      DataColumn(label: Text('Image')),
+      DataColumn(label: Text('Categorie')),
+      DataColumn(label: Text('Quantité')),
+      DataColumn(label: Text('Date')),
+      DataColumn(label: Text('Action')),
+    ]);
 
     // Récupérer la largeur de l'écran
     double screenWidth = screenSize.width;
 
     // Récupérer la hauteur de l'écran
     double screenHeight = screenSize.height;
-    return Row(
-      children: [
-        Expanded(
-          flex: 10,
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 40),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: "Recherche",
-                    labelStyle: TextStyle(color: primaryColor),
-                    filled: true,
-                    fillColor: secondaryColor,
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: primaryColor),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: secondaryColor),
-                      borderRadius: BorderRadius.circular(10),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Row(
+        children: [
+          Expanded(
+            flex: 10,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: "Recherche",
+                        labelStyle: TextStyle(color: primaryColor),
+                        filled: true,
+                        fillColor: secondaryColor,
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: primaryColor),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: secondaryColor),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              Column(
-                children: [
-                  Container(
-                      width: screenHeight * 1.5,
-                      height: 80,
-                      color: textSecondaryColor,
-                      child: Row(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              'Produits',
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          SizedBox(width: 780),
-                          InkWell(
-                            onTap: () {
-                              print('nouveau');
-                              __ShowPopup(context);
-                            },
-                            child: Container(
-                              color: primaryColor,
-                              height: 27,
-                              width: 110,
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    Icons.add,
-                                    color: Colors.white,
-                                  ),
-                                  Text(
-                                    'Nouveau',
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 18),
-                                  )
-                                ],
+                  Column(
+                    children: [
+                      Container(
+                          width: screenHeight * 1.5,
+                          height: 80,
+                          color: pageBackgroundColor,
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  'Produits',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
                               ),
-                            ),
-                          )
-                        ],
-                      )),
+                              SizedBox(width: 780),
+                              InkWell(
+                                onTap: () {
+                                  print('nouveau');
+                                  __ShowPopup(context);
+                                },
+                                child: Container(
+                                  color: primaryColor,
+                                  height: 27,
+                                  width: 110,
+                                  child: Row(
+                                    children: [
+                                      Icon(
+                                        Icons.add,
+                                        color: Colors.white,
+                                      ),
+                                      Text(
+                                        'Nouveau',
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 18),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          )),
+                    ],
+                  ),
+                  DataTable(
+                    columns: EnteteTableau,
+                    rows: [
+                      DataRow(cells: Contenu),
+                      DataRow(cells: Contenu),
+                      DataRow(cells: Contenu),
+                      DataRow(cells: Contenu),
+                      DataRow(cells: Contenu),
+                      DataRow(cells: Contenu),
+                      DataRow(cells: Contenu),
+                      DataRow(cells: Contenu),
+                      DataRow(cells: Contenu),
+                      DataRow(cells: Contenu)
+                    ],
+
+                    //infoproducts
+                    //.map((Info) => DataRow(cells: [
+                    // DataCell(Text(Info.ID)),
+                    // DataCell(Text(Info.Image)),
+                    // DataCell(Text(Info.Categorie)),
+                    //DataCell(Text('Info.Quantite')),
+                    //DataCell(Text(Info.Date)),
+                    //DataCell(Info.Action as Widget),
+                    //]))
+                    //.toList(),
+                    columnSpacing: 130,
+                  )
                 ],
               ),
-            ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
@@ -109,5 +188,55 @@ class AcceuilScreenProductsState extends State<AcceuilScreenProducts> {
         builder: (BuildContext context) {
           return AddProducts();
         });
+  }
+}
+
+class InfoProducts {
+  String ID;
+  String Image;
+  String Categorie;
+  int Quantite;
+  String Date;
+  Icon Action;
+
+  InfoProducts(
+      {required this.ID,
+      required this.Image,
+      required this.Categorie,
+      required this.Quantite,
+      required this.Date,
+      required this.Action});
+
+  static List<InfoProducts> getInfoProducts() {
+    return <InfoProducts>[
+      InfoProducts(
+          ID: '1',
+          Image: 'gdgdf',
+          Categorie: 'ddd',
+          Quantite: 2,
+          Date: '',
+          Action: Icon(Icons.edit)),
+      InfoProducts(
+          ID: '1',
+          Image: 'dfd',
+          Categorie: 'dfdf',
+          Quantite: 2,
+          Date: 'dfdf',
+          Action: Icon(Icons.edit)),
+      InfoProducts(
+          ID: '1',
+          Image: 'dfdf',
+          Categorie: 'dddqs',
+          Quantite: 2,
+          Date: 'drtt',
+          Action: Icon(Icons.edit)),
+      InfoProducts(
+          ID: '1',
+          Image: 'ytt',
+          Categorie: 'ggg',
+          Quantite: 2,
+          Date: 'ggggg',
+          Action: Icon(Icons.edit)),
+    ];
   }
 }
